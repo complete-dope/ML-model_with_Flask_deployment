2bc45357bed2473c56b57489213262d8a6ec1a5b
complete-dope <mdulani2002@gmail.com>
2022-03-20 13:49:54 +0530

    good

--- Customer retention/api_call_handle.py
+++
@@ -1,6 +0,0 @@
-import requests
-# we can check this on postman by inserting our content in body field in the json format. 
-url="https://localhost:5000/apicall"
-
-concat = requests.post(url , json())
-print(concat.json())
